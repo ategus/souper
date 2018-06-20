c2159ae2b8f9ade03ee945e4d4c98cda7e8b26a4
= <=ronny.schellenberg@gmail.com>
2018-06-20 18:12:59 +0200

    price grabbing works

--- soup.py
+++ soup.py
@@ -1,6 +1,7 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import re
 
 myurl = 'https://www.cyberport.de/gaming/gaming-monitore.html'
 
@@ -8,9 +9,30 @@ uClient = uReq(myurl)
 page_html = uClient.read()
 uClient.close()
 
+#html parser
 page_soup = soup(page_html, "html.parser")
-#print(page_soup.body.span
 
-result = page_soup.find_all("div",{"class":"productWrapper"})
+#grab container
+containers = page_soup.find_all("div",{"class":"productWrapper"})
+#containers = page_soup.find_all("article")
 
-print(result)
+container_count = len(containers)
+
+contain = containers[0]
+price = contain.find_all("div",{"class":"price orange"})
+#price = contain.find_all("span")
+
+price
+
+result = re.split('\s+', str(price))
+
+
+print (result[6])
+#print(containers)
+#print(contain)
+
+#print (container_count)
+
+#logging.info(contain)
+#print (containers[0]    )
+#print(result)
