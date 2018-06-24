dcaed646a006c55b32a83daecace9f9009225b6b
= <=ronny.schellenberg@gmail.com>
2018-06-24 12:57:42 +0200

    cfg File

--- selen.py
+++ selen.py
@@ -10,6 +10,8 @@ import bs4
 #from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import re
+import pandas as pd
+import sys
 
 
 browser = webdriver.Chrome()
@@ -38,27 +40,47 @@ element_send.click()
 
 #element_Kunden = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.LINK_TEXT, 'Kunden')))
 
-#wait = WebDriverWait(browser, 30)
-#element_kunden = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Kunden')))
-
 time.sleep(13)
+
+soup_level1=soup(browser.page_source, 'lxml')
+
 
 browser.find_element_by_link_text("Kunden").click()
 time.sleep(1)
+#suchen
 browser.find_element_by_id("s_2_1_42_0_Ctrl").click()
 time.sleep(1)
+#show more results
+browser.find_element_by_id("s_1_1_4_0_mb").click()
+time.sleep(1)
 
-#data = []
-#for tr in browser.find_element_by_id('s_1_l'):
-#    tds = tr.find_element_by_id('td')
-#    if tds: 
-#        data.append([td.text for td in tds])
-#print(data)
+#next
 
-soup_level1=soup(browser.page_source, 'lxml')
-print (browser.page_source)
+for a in range (45):
+    browser.find_element_by_id("last_pager_s_1_l").click()
+    #WebDriverWait(browser,10,poll_frequency=0.5).until(EC.element_to_be_clickable((By.ID, 'last_pager_s_1_l'))).click()
+    time.sleep(2)
+
+            
+
+
+
+'''
+data = []
+for tr in browser.find_element_by_id('s_1_l'):
+    tds = tr.find_element_by_id('td')
+    if tds: 
+        data.append([td.text for td in tds])
+print(data)
+'''
+
+#print (browser.page_source)
 #table = soup_level1.find_all('table')[0]
 
 #print(table)
+
+
+
+
 
 #browser.quit()
